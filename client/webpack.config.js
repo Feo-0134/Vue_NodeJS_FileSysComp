fbc6ce2fd0a76075a8c680dccd2adeef06cd80ad
Frankie34 <zhujuncheng1021@163.com>
2019-10-15 13:31:22 +0800

    init webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,18 @@
+modules.export={
+    entry:{
+        /* 入口文件 */
+    },
+    output:{
+        /* 出口文件 */
+    },
+    module:{
+        /* Loader */
+        rules:[{},{},{}]
+    },
+    plugins:[ 
+        /* 插件 */ 
+    ],
+    devtool: {},
+    devServer: {},
+    resolve:{}
+}
