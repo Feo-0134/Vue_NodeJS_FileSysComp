05ccb89778dd57cb5ab4fa6a1e4c39eed5fd1f24
Frankie34 <zhujuncheng1021@163.com>
2019-10-16 10:44:45 +0800

    Fix Bug: Koa 使用 CORS 从后端解决允许跨域请求

--- client/src/components/UploadPage.vue
+++ client/src/components/UploadPage.vue
@@ -1,58 +1,60 @@
 <template>
   <div class="hello">
     <el-button @click='uploadFile'>Biu</el-button>
-    <form action="/" method="post" enctype="multipart/form-data">
+    <!-- <form action="/" method="post" enctype="multipart/form-data">
       <input type="file" name="file" multiple>
       <input type="submit" value="Upload" @click=";">
-    </form>
+    </form> -->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'UploadPage',
-  props: {
-    msg: String
-  },
-  methods: {
-    uploadFile() {
-        // this.$http.get(this.apiPath)
-        this.$http.post(this.apiPath, this.apiPayload)
-    }
-  },
-  computed:{
-    apiPath() {
-      return (
-        'http://127.0.0.1:3000/api/upload'
-      );
+  export default {
+    name: 'UploadPage',
+    props: {
+      msg: String
     },
-    apiPayload() {
-      return {
-        files:{
-          file:{
-            path:'C:/Users/t-junzhu/workspace/helloWorld.txt'
+    methods: {
+      uploadFile() {
+          // this.$http.get(this.apiPath)
+          this.$http.post(this.apiPath, this.apiPayload)
+          console.log(this.apiPayload)
+      }
+    },
+    computed:{
+      apiPath() {
+        return (
+          'http://127.0.0.1:3000/api/upload'
+        );
+      },
+      apiPayload() {
+        return {
+          files:{
+            file:{
+              path:'C:/Users/t-junzhu/workspace/helloWorld.txt',
+              name:'helloWorld.txt'
+            }
           }
         }
       }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
--- server.js
+++ server.js
@@ -3,46 +3,55 @@ const serve = require('koa-static');
 const koaBody = require('koa-body');
 const Router = require('koa-router');
 const cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser')
 const router = new Router({ prefix: '/api' })
 const Koa = require('koa');
 const fs = require('fs');
 const app = new Koa();
 const os = require('os');
 const path = require('path');
+const URL = require('url');
 
 // log requests
 
-app.use(logger());
+// app.use(logger());
 
-app.use(koaBody({ multipart: true }));
+// app.use(koaBody({ multipart: true }));
 
 // serve files from ./public
 // app.use(serve(path.join(__dirname, '/public')));
 
+
 // handle uploads
 const upload = async function (ctx) {
+  console.log(ctx.request.body)
   const file = ctx.request.body.files.file;
-  console.log(file)
   const reader = fs.createReadStream(file.path);
-  const stream = fs.createWriteStream(path.join(os.tmpdir(), Math.random().toString()));
+  const stream = fs.createWriteStream(path.join(os.tmpdir(), 'uploadfile' + Math.random().toString()));
   reader.pipe(stream);
   console.log('uploading %s -> %s', file.name, stream.path);
 }
 
 router.post('/upload', upload)
 
-app.use(router.routes())
-   .use(router.allowedMethods())
-   .use(cors({
-    origin: function (ctx) {
-      return "*";
-    },
-    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-    maxAge: 5,
-    credentials: true,
-    allowMethods: ['GET', 'POST', 'DELETE', 'PUT'],
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'accesstoken'],
-  }))
+app.use(cors({
+  origin: function (ctx) {
+      if (ctx.url === '/test') {
+          return "*"; // 允许来自所有域名请求
+      }
+      return 'http://localhost:8080'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+
+app
+    .use(bodyParser())
+    .use(router.routes())
+    .use(router.allowedMethods())
 
 // listen
 
